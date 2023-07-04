dbee231ad120b46aea999f2289b462f46b2f03b7
Taco <wangziqi2002vip@163.com>
2023-07-04 13:31:25 +0800

    fine

--- openAi-api/Example/message_with_conversation.py
+++ openAi-api/Example/message_with_conversation.py
@@ -1,14 +1,18 @@
 import openai
 
-openai.api_key = 'your-api-key'
+openai.api_key = ''
 
 response = openai.Completion.create(
   engine="text-davinci-003.5",  # or "text-davinci-004" for GPT-4
+  # messages=[
+  #       {"role": "system", "content": "You are a helpful assistant."},
+  #       {"role": "user", "content": "Who won the world series in 2020?"},
+  #       {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
+  #       {"role": "user", "content": "Where was it played?"}
+  #   ]
   messages=[
-        {"role": "system", "content": "You are a helpful assistant."},
-        {"role": "user", "content": "Who won the world series in 2020?"},
-        {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
-        {"role": "user", "content": "Where was it played?"}
+        {"role": "system", "content": "You are a career planning assistant."},
+        {"role": "user", "content": "I have a degree in Computer Science, 2 years of experience in software development, and I'm proficient in Python and Java. I want to apply for the position of Senior Software Engineer at Google."},
     ]
 )
 
