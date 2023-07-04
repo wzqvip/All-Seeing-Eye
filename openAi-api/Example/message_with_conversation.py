e7ca409d943769e8ab881ec149b40be1da30359b
Taco <wangziqi2002vip@163.com>
2023-07-04 13:52:52 +0800

    example.

--- openAi-api/Example/message_with_conversation.py
+++ openAi-api/Example/message_with_conversation.py
@@ -1,19 +1,24 @@
 import openai
+import os
+import json
 
-openai.api_key = ''
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
-response = openai.Completion.create(
-  engine="text-davinci-003.5",  # or "text-davinci-004" for GPT-4
-  # messages=[
-  #       {"role": "system", "content": "You are a helpful assistant."},
-  #       {"role": "user", "content": "Who won the world series in 2020?"},
-  #       {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
-  #       {"role": "user", "content": "Where was it played?"}
-  #   ]
-  messages=[
-        {"role": "system", "content": "You are a career planning assistant."},
-        {"role": "user", "content": "I have a degree in Computer Science, 2 years of experience in software development, and I'm proficient in Python and Java. I want to apply for the position of Senior Software Engineer at Google."},
+response = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role": "system", "content": "You are a helpful career planning assistant.."},
+        {"role": "user", "content": "I want to apply for a job, can you offer me some help?"},
+        {"role": "assistant", "content": "Sure. Please Describe yourself and the job you are going to acquire. I will give you the chance you will be accepted and the similar jobs."},
+        {"role": "user", "content": "I have a degree in Computer Science, 2 years of experience in software development, and I'm proficient in Python and Java. I want to apply for the position of Senior Software Engineer at Google."}
     ]
 )
+# messages=[
+#     {"role": "system", "content": "You are a career planning assistant."},
+#     {"role": "user", "content": "I have a degree in Computer Science, 2 years of experience in software development, and I'm proficient in Python and Java. I want to apply for the position of Senior Software Engineer at Google."}
+# ]
 
 print(response['choices'][0]['message']['content'])
+
+with open('response.json', 'w') as f:
+    json.dump(response, f)
