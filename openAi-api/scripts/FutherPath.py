215b2bf846f7ee3ed1f62d2ec388bf4879cad18f
Taco <wangziqi2002vip@163.com>
2023-07-05 14:52:41 +0800

    futurePath

--- openAi-api/scripts/FutherPath.py
+++ openAi-api/scripts/FutherPath.py
@@ -4,13 +4,14 @@ import json
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-user_description = "I'm a 30-year-old software engineer living in San Francisco. I'm passionate about technology and love to travel. What does my future look like?"
+# user_description = "I'm a 30-year-old software engineer living in San Francisco. I'm passionate about technology and love to travel. What does my future look like?"
 # user_description= "I'm graduated from elementary school but I have doctor's degree. I want a job as Presendent."
+user_description = "I'm a CS graduated student, I have less money. I live in China, I'm longing for freedom in USA. I want a big house and a cute wife."
 
 response = openai.ChatCompletion.create(
     model="gpt-3.5-turbo-0301",
     messages=[
-          {"role": "system", "content": "You are a fortune teller.You should tell user what he will be in the futher, and what his life will be like."},
+          {"role": "system", "content": "You are a fortune teller.You should tell user what he will be in the futher, and what his life will be like. You don't need to give any summary. Just describe the user's life in the futher in time order."},
         {"role": "user", "content": user_description}
     ]
 
