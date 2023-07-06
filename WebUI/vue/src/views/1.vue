4b7d7a1ecabeb035be48e25688878ad9814d2df1
Taco <wangziqi2002vip@163.com>
2023-07-06 20:19:40 +0800

    Update 1.vue

--- WebUI/vue/src/views/1.vue
+++ WebUI/vue/src/views/1.vue
@@ -6,6 +6,16 @@
     <el-button @click="submitInput(2)">给出应聘建议</el-button>
     <el-button @click="submitInput(3)">预测未来</el-button>
     <div v-if="response">{{ response }}</div>
+
+    <h3>示例输入</h3>
+    <p>预测应聘成功概率示例：</p>
+    <p>I have a degree in Computer Science, 2 years of experience in software development, and I'm proficient in Python and Java. I want to apply for the position of Senior Software Engineer at Google.</p>
+    <p>I have ten years of driving experience. I want to be a taxi driver to earn a living. Will I succeed?</p>
+    <p>提供应聘指导建议示例：</p>
+    <p>I'm an experienced programmer, software developer. I have been worked at Google, Microsoft. I want to apply for a job as senior software developer at Amazon.</p>
+    <p>预测人生示例：</p>
+    <p>I'm a 30-year-old software engineer living in San Francisco. I'm passionate about technology and love to travel. What does my future look like?</p>
+    <p>I'm a CS graduated student, I have less money. I live in China, I'm longing for freedom in USA.</p>
   </div>
 </template>
 
