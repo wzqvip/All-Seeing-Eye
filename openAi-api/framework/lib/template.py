bb4be05df3744db68d73c9e1ae21622a0d2267c8
wzqvip <tacoin@shanghaitech.edu.cn>
2023-07-09 17:52:35 +0800

    rename

--- openAi-api/framework/lib/template.py
+++
@@ -1,6 +0,0 @@
-import openai
-class GPTtemplate:
-    def __init__(self,API_KEY):
-        openai.api_key = API_KEY
-        self.goals = {} # goal is a dictionary of {goal prompt:finish percent}
-        
--- openAi-api/framework/tests/get.py
+++
@@ -1,9 +0,0 @@
-
-import os
-import openai
-
-
-
-openai.organization = "org-YRHuMSPzIql3xRZ0DbuYLyK1"
-openai.api_key = ""
-print(openai.Model.list())
